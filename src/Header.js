5d51a72bf37f3403982e320c49f6334418b990bb
+ <+>
2021-10-26 16:47:05 -0400

    Updated RideCard

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function Header() {
-    return (
-       <div>
-           <h2>This is the header</h2>
-       </div>
-    );
+  return (
+    <div>
+      <h2>This is the header</h2>
+      <img
+        src="https://chicagoinnovation.com/wp-content/uploads/2021/02/Flatiron-logo.png"
+        alt="Flatiron Logo"
+      />
+    </div>
+  );
 }
 
 export default Header;
--- src/RideCard.js
+++ src/RideCard.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
-function RideCard() {
-    return (
-       <div>
-           <p>I am your rides</p>
-       </div>
-    );
+function RideCard({ first_name, last_name, distance, amount, tipped }) {
+  return (
+    <div>
+      <p>I am your rides</p>
+      <h2>
+        `${first_name} ${last_name}`
+      </h2>
+      <h2>Distance: `${distance}`</h2>
+      <h2>Amount: `${amount}`</h2>
+      <h2>Tipped: `${tipped}`</h2>
+    </div>
+  );
 }
 
 export default RideCard;
